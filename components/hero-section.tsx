efb133606b7bd6da0c0ee6caca246ecd3e30515c
jp-a29 <johnpatrickpalmesalpas@gmail.com>
2026-02-13 23:22:37 +0800

    commit

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -7,26 +7,6 @@ import { TextEffect } from '@/components/ui/text-effect'
 import { AnimatedGroup } from '@/components/ui/animated-group'
 import { HeroHeader } from './header'
 
-const transitionVariants = {
-    item: {
-        hidden: {
-            opacity: 0,
-            filter: 'blur(12px)',
-            y: 12,
-        },
-        visible: {
-            opacity: 1,
-            filter: 'blur(0px)',
-            y: 0,
-            transition: {
-                type: 'spring',
-                bounce: 0.3,
-                duration: 1.5,
-            },
-        },
-    },
-}
-
 export default function HeroSection() {
     return (
         <>
@@ -83,7 +63,31 @@ export default function HeroSection() {
 
                         <div className="mx-auto max-w-7xl px-6">
                             <div className="text-center sm:mx-auto lg:mr-auto lg:mt-0">
-                                <AnimatedGroup variants={transitionVariants}>
+                                <AnimatedGroup
+                                    variants={{
+                                        container: {
+                                            visible: {
+                                                transition: {
+                                                    delayChildren: 0.2,
+                                                },
+                                            },
+                                        },
+                                        item: {
+                                            hidden: {
+                                                opacity: 0,
+                                                y: 10,
+                                            },
+                                            visible: {
+                                                opacity: 1,
+                                                y: 0,
+                                                transition: {
+                                                    type: 'spring',
+                                                    bounce: 0.3,
+                                                    duration: 0.8,
+                                                },
+                                            },
+                                        },
+                                    }}>
                                     <Link
                                         href="#link"
                                         className="hover:bg-background dark:hover:border-t-border bg-muted group mx-auto flex w-fit items-center gap-4 rounded-full border p-1 pl-4 shadow-md shadow-zinc-950/5 transition-colors duration-300 dark:border-t-white/5 dark:shadow-zinc-950">
@@ -130,12 +134,24 @@ export default function HeroSection() {
                                                 },
                                             },
                                         },
-                                        ...transitionVariants,
+                                        item: {
+                                            hidden: {
+                                                opacity: 0,
+                                                y: 12,
+                                            },
+                                            visible: {
+                                                opacity: 1,
+                                                y: 0,
+                                                transition: {
+                                                    type: 'spring',
+                                                    bounce: 0.3,
+                                                    duration: 1.5,
+                                                },
+                                            },
+                                        },
                                     }}
                                     className="mt-12 flex flex-col items-center justify-center gap-2 md:flex-row">
-                                    <div
-                                        key={1}
-                                        className="bg-foreground/10 rounded-[calc(var(--radius-xl)+0.125rem)] border p-0.5">
+                                    <div className="bg-foreground/10 rounded-[calc(var(--radius-xl)+0.125rem)] border p-0.5">
                                         <Button
                                             asChild
                                             size="lg"
@@ -146,7 +162,6 @@ export default function HeroSection() {
                                         </Button>
                                     </div>
                                     <Button
-                                        key={2}
                                         asChild
                                         size="lg"
                                         variant="ghost"
@@ -169,7 +184,21 @@ export default function HeroSection() {
                                         },
                                     },
                                 },
-                                ...transitionVariants,
+                                item: {
+                                    hidden: {
+                                        opacity: 0,
+                                        y: 12,
+                                    },
+                                    visible: {
+                                        opacity: 1,
+                                        y: 0,
+                                        transition: {
+                                            type: 'spring',
+                                            bounce: 0.3,
+                                            duration: 1.5,
+                                        },
+                                    },
+                                },
                             }}>
                             <div className="mask-b-from-55% relative -mr-56 mt-8 overflow-hidden px-2 sm:mr-0 sm:mt-12 md:mt-20">
                                 <div className="inset-shadow-2xs ring-background dark:inset-shadow-white/20 bg-background relative mx-auto max-w-6xl overflow-hidden rounded-2xl border p-4 shadow-lg shadow-zinc-950/15 ring-1">
